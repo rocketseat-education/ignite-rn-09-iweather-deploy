68af6dcadc7e580edb48d60fca9a02c77f667c25
RennanD <rplaykl@gmail.com>
2023-08-10 14:09:29 -0300

    CI: Corrigindo testes

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -23,8 +23,7 @@ describe("Screen: Dashboard", () => {
 
     render(<Dashboard />)
 
-    const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
-    expect(cityName).toBeTruthy() 
+    await waitFor(() => expect(screen.findByText(/rio do sul/i, {}, { timeout: 3000 })).toBeTruthy);
   })
 
   it('should be show another selected weather city', async () =>{
